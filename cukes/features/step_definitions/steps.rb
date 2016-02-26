874b86814c95d89e2b913412eaad918dfbfcb412
Yash Kulshrestha <akulshr@ra.rockwell.com>
2016-02-25 19:32:16 -0500

    changed the tabbing on steps

--- cukes/features/step_definitions/steps.rb
+++ cukes/features/step_definitions/steps.rb
@@ -1,11 +1,11 @@
 Given(/^that I'm at the promises homepage$/) do
-  visit PromisesPage
+    visit PromisesPage
 end
 
 When(/^I click on Simple Handling$/) do
-  on(PromisesPage).simple_handling
+    on(PromisesPage).simple_handling
 end
 
 Then(/^I should see the simple handling page$/) do
-  expect(on(SimpleHandlingPage).loaded?).to be true
+    expect(on(SimpleHandlingPage).loaded?).to be true
 end
