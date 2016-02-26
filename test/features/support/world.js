9f73ed89797303e055c60ebfcdb68b53ba4ec14d
Kyle McKee <McKee.Kyle@gmail.com>
2016-02-26 07:02:28 -0500

    Fixed window size to prevent responsiveness from causing differences on different machines.

--- test/features/support/world.js
+++ test/features/support/world.js
@@ -3,6 +3,7 @@ var webdriver = require('webdriver-sync');
 
 function World() {
     this.browser = new webdriver.ChromeDriver();
+    this.browser.manage().window().setSize(new webdriver.Dimension(800, 800));
 }
 
 module.exports = function() {
